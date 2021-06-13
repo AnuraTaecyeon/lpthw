aba73b60896c2652be98abff9a27c34775fe1726
AnuraTaecyeon <85743429+AnuraTaecyeon@users.noreply.github.com>
2021-06-13 18:33:49 +0545

    Update ex1.py
    there are also comment samples

--- ex1.py
+++ ex1.py
@@ -1,3 +1,25 @@
+# what did I learn today?
+# Print
+# to give instructions to Computer through Atom (text editor) to print some text
+
+# Comment
+# is a new word for me as the computer only understands the computer language.
+# Strictly to understand that it does not show any output at Powershell or Github
+# Comment is not read by computer
+
+# Save
+# essential to save the command at text editor to see the output at Powershell
+
+# push
+# Once the file is save it is require to push the file to show it at Github
+
+# Powershell
+# It is where you find the documents and files. Also see the output while given instructions
+
+# command
+# Computer language to run the program
+
+
 print (" Hello World!")
 print ("Hello Again")
 print ("I like typing this.")
@@ -5,3 +27,5 @@ print ("This is fun.")
 print ('Yay! Printing.')
 print ("I'd much rather you 'not'")
 print ('I "said" do not touch this.')
+
+# print("i am having tea")
